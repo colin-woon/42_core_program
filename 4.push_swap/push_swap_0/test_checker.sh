a2a42587a4e776e2c350ca7881b3137134e07b0f
Colin Woon <colin.wyj@gmail.com>
2024-09-10 19:34:04 +0800

    Updated tests

--- 4.push_swap/push_swap_0/test_checker.sh
+++ 4.push_swap/push_swap_0/test_checker.sh
@@ -2,56 +2,56 @@
 
 echo "CONDITION ac > 1 && ac <=3"
 echo "Normal OK"
-./push_swap "1 2 3" | ./checker -v "1 2 3"
-./push_swap "3 2 1" | ./checker -v "3 2 1"
-./push_swap "1 2 3" | ./checker "1 2 3" -v
-./push_swap "3 2 1" | ./checker "3 2 1" -v
-./push_swap "7 6 5 4 3 2 1" | ./checker "7 6 5 4 3 2 1" -v
-./push_swap "7 6 5 4 3 2 1" | ./checker -v "7 6 5 4 3 2 1"
+./push_swap "1 2 3" | valgrind -s --leak-check=full ./checker -c "1 2 3"
+./push_swap "3 2 1" | valgrind -s --leak-check=full ./checker -c "3 2 1"
+./push_swap "1 2 3" | valgrind -s --leak-check=full ./checker "1 2 3" -c
+./push_swap "3 2 1" | valgrind -s --leak-check=full ./checker "3 2 1" -c
+./push_swap "7 6 5 4 3 2 1" | valgrind -s --leak-check=full ./checker "7 6 5 4 3 2 1" -c
+./push_swap "7 6 5 4 3 2 1" | valgrind -s --leak-check=full ./checker -c "7 6 5 4 3 2 1"
 echo ""
 
-echo "Coloured OK"
-./push_swap "1 2 3" | ./checker "1 2 3"
-./push_swap "3 2 1" | ./checker "3 2 1"
-echo ""
+# echo "Coloured OK"
+# ./push_swap "1 2 3" | valgrind -s --leak-check=full ./checker "1 2 3"
+# ./push_swap "3 2 1" | valgrind -s --leak-check=full ./checker "3 2 1"
+# echo ""
 
-echo "Should be Error"
-./push_swap "1 2 3" | ./checker "-v 1 2 3"
-./push_swap "1 2 3" | ./checker "1 2 3 -v"
-./push_swap "1 2 3" | ./checker "1 2 -v 3"
-./push_swap "1 2 3" | ./checker "-v1 2 3"
-./push_swap "1 2 3" | ./checker "1-v 2 3"
-./push_swap "1 2 3" | ./checker "1 2 3-v"
-./push_swap "1 2 3" | ./checker "1 2 -v3"
-./push_swap "1 2 3" | ./checker "1 2-v 3"
-./push_swap "1 2 3" | ./checker "1 -v2 3"
-echo ""
+# echo "Should be Error"
+# ./push_swap "1 2 3" | valgrind -s --leak-check=full ./checker "-c 1 2 3"
+# ./push_swap "1 2 3" | valgrind -s --leak-check=full ./checker "1 2 3 -c"
+# ./push_swap "1 2 3" | valgrind -s --leak-check=full ./checker "1 2 -c 3"
+# ./push_swap "1 2 3" | valgrind -s --leak-check=full ./checker "-c1 2 3"
+# ./push_swap "1 2 3" | valgrind -s --leak-check=full ./checker "1-c 2 3"
+# ./push_swap "1 2 3" | valgrind -s --leak-check=full ./checker "1 2 3-c"
+# ./push_swap "1 2 3" | valgrind -s --leak-check=full ./checker "1 2 -c3"
+# ./push_swap "1 2 3" | valgrind -s --leak-check=full ./checker "1 2-c 3"
+# ./push_swap "1 2 3" | valgrind -s --leak-check=full ./checker "1 -c2 3"
+# echo ""
 
-echo "CONDITION ac > 3"
-echo "Normal OK"
-./push_swap 3 2 1 | ./checker -v 3 2 1
-./push_swap 3 2 1 | ./checker 3 2 1 -v
-./push_swap 1 2 3 | ./checker 1 2 3 -v
-./push_swap 1 2 3 | ./checker -v 1 2 3
-echo ""
+# echo "CONDITION ac > 3"
+# echo "Normal OK"
+# ./push_swap 3 2 1 | valgrind -s --leak-check=full ./checker -c 3 2 1
+# ./push_swap 3 2 1 | valgrind -s --leak-check=full ./checker 3 2 1 -c
+# ./push_swap 1 2 3 | valgrind -s --leak-check=full ./checker 1 2 3 -c
+# ./push_swap 1 2 3 | valgrind -s --leak-check=full ./checker -c 1 2 3
+# echo ""
 
-echo "Should be Error"
-./push_swap 3 2 1 | ./checker 3-v 2 1
-./push_swap 3 2 1 | ./checker -v3 2 1
-./push_swap 3 2 1 | ./checker 3 -v2 1
-./push_swap 3 2 1 | ./checker 3 2 -1c
-./push_swap 3 2 1 | ./checker 3 2 -v1
-./push_swap 3 2 1 | ./checker 3 2 1-v
-echo ""
+# echo "Should be Error"
+# ./push_swap 3 2 1 | valgrind -s --leak-check=full ./checker 3-c 2 1
+# ./push_swap 3 2 1 | valgrind -s --leak-check=full ./checker -c3 2 1
+# ./push_swap 3 2 1 | valgrind -s --leak-check=full ./checker 3 -c2 1
+# ./push_swap 3 2 1 | valgrind -s --leak-check=full ./checker 3 2 -1c
+# ./push_swap 3 2 1 | valgrind -s --leak-check=full ./checker 3 2 -c1
+# ./push_swap 3 2 1 | valgrind -s --leak-check=full ./checker 3 2 1-c
+# echo ""
 
-echo "Should be Error when 2 flags"
-./push_swap 3 2 1 | ./checker 3 2 1-vv
-./push_swap 3 2 1 | ./checker 3 2 1-vc
-./push_swap 3 2 1 | ./checker 3 2 1 -v -v
-./push_swap 3 2 1 | ./checker 3 2 1 -v -v
-./push_swap 3 2 1 | ./checker -v -v 3 2 1
-./push_swap 3 2 1 | ./checker -v -v 3 2 1
-./push_swap 3 2 1 | ./checker -vc 3 2 1
-./push_swap 3 2 1 | ./checker -vv 3 2 1
-./push_swap 3 2 1 | ./checker 3 -v -v 2 1
-echo ""
+# echo "Should be Error when 2 flags"
+# ./push_swap 3 2 1 | valgrind -s --leak-check=full ./checker 3 2 1-cv
+# ./push_swap 3 2 1 | valgrind -s --leak-check=full ./checker 3 2 1-cc
+# ./push_swap 3 2 1 | valgrind -s --leak-check=full ./checker 3 2 1 -c -c
+# ./push_swap 3 2 1 | valgrind -s --leak-check=full ./checker 3 2 1 -c -c
+# ./push_swap 3 2 1 | valgrind -s --leak-check=full ./checker -c -c 3 2 1
+# ./push_swap 3 2 1 | valgrind -s --leak-check=full ./checker -c -c 3 2 1
+# ./push_swap 3 2 1 | valgrind -s --leak-check=full ./checker -cc 3 2 1
+# ./push_swap 3 2 1 | valgrind -s --leak-check=full ./checker -cv 3 2 1
+# ./push_swap 3 2 1 | valgrind -s --leak-check=full ./checker 3 -c -c 2 1
+# echo ""
